9e1a58bd579b92e500d5b25d038e93e4f25b2877
priscilarosa <priscilarosa@MacBook-Pro-de-Priscila.local>
2016-10-05 09:00:39 -0300

    collection

---
+++ multipeer/Cell.swift
@@ -0,0 +1,16 @@
+//
+//  Cell.swift
+//  multipeer
+//
+//  Created by Priscila Rosa on 10/5/16.
+//  Copyright © 2016 Isabella Vieira. All rights reserved.
+//
+
+import UIKit
+
+class Cell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+
+}
--- multipeer/ViewController.swift
+++ multipeer/ViewController.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var images:[UIImage] = []
 
+    @IBOutlet weak var collection: UICollectionView!
+    
+    @IBAction func cameraButton(_ sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = UIImagePickerControllerSourceType.camera;
+            picker.allowsEditing = false
+            
+            self.present(picker, animated: true, completion: nil)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,8 +36,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    
-    //COMMIT
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return images.count
+        
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath as IndexPath) as! Cell
+        cell.imageView.image = self.images[indexPath.row]
+        
+        return cell
+    }
 
 }
 
